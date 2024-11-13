7fa54ed7d5eb4e1abf629fc449c7d37532e5c32e
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-11-12 19:52:47 -0500

    program counts down from 4

--- main.ts
+++ main.ts
@@ -2,7 +2,20 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program counts down from 4 and lights up neopixels accordingly
 */
 
-basic.showString('Hello, World!')
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+let loopcounter = 4
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+
+while (loopcounter >= 0) {
+    basic.showNumber (loopcounter)
+    strip.range(0, loopcounter).showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(1000)
+    loopcounter = loopcounter - 1
+}
+})
+basic.showIcon (IconNames.Happy)
